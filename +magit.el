585be90b3452c38855afce42ece9fe1b4b426f4f
Jonathan D. Chang <6375175+jdkschang@users.noreply.github.com>
2019-04-12 13:47:39 -0700

    gpg key update

--- +magit.el
+++ +magit.el
@@ -1,20 +1,20 @@
 ;;; ~/dotfiles/dots/emacs/doom/+magit.el -*- lexical-binding: t; -*-
 
 (after! magit
-  (magit-todos-mode nil)
-  (magit-define-popup-option 'magit-rebase-popup
-    ?S "Sign using gpg" "--gpg-sign=" #'magit-read-gpg-secret-key)
-  (setq magit-save-repository-buffers nil
-        magit-repository-directories '(("~/org/projects" . 3)
-                                       ("~/src" . 3)
-                                       ("~/.emacs.d"))
-        magit-commit-arguments '("--gpg-sign=703A05F567070F95")
-        magit-rebase-arguments '("--autosquash" "--gpg-sign=703A05F567070F95")
-        magit-pull-arguments   '("--rebase" "--gpg-sign=703A05F567070F95")
-        +magit-hub-features t ;; I want the PR/issue stuff too!
-        +magit-hub-enable-by-default t))  ;; And I want it on by default!
+	(magit-todos-mode nil)
+	(magit-define-popup-option 'magit-rebase-popup
+		?S "Sign using gpg" "--gpg-sign=" #'magit-read-gpg-secret-key)
+	(setq magit-save-repository-buffers nil
+				magit-repository-directories '(("~/org/projects" . 3)
+																			 ("~/src" . 3)
+																			 ("~/.emacs.d"))
+				magit-commit-arguments '("--gpg-sign=809A1C18A92416A")
+				magit-rebase-arguments '("--autosquash" "--gpg-sign=809A1C18A92416A")
+				magit-pull-arguments   '("--rebase" "--gpg-sign=809A1C18A92416A")
+				+magit-hub-features t ;; I want the PR/issue stuff too!
+				+magit-hub-enable-by-default t))  ;; And I want it on by default!
 
 (after! magithub
-  (setq magithub-preferred-remote-method 'git_url
-        ;; magithub-clone-default-directory "~/Dropbox/org/projects" ;; I want my stuff to clone to ~/projects
-        magithub-clone-default-directory "~/src/github.com"))
+	(setq magithub-preferred-remote-method 'git_url
+				;; magithub-clone-default-directory "~/Dropbox/org/projects" ;; I want my stuff to clone to ~/projects
+				magithub-clone-default-directory "~/src/github.com"))
