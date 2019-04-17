46313768b3d88623c44c0d4bc2569ce302c9850c
Jonathan D. Chang <6375175+jdkschang@users.noreply.github.com>
2019-04-17 09:48:17 -0700

    merging

--- +bindings.el
+++ +bindings.el
@@ -19,39 +19,39 @@
  :n "C-l" 'evil-window-right
 
  (:map evil-treemacs-state-map
-	 "C-h" 'evil-window-left
-	 "C-l" 'evil-window-right)
+   "C-h" 'evil-window-left
+   "C-l" 'evil-window-right)
 
  (:when (featurep! :feature workspaces)
-	 :nmvi "s-t" #'+workspace/new
-	 :nmvi "s-1" (λ! (+workspace/switch-to 0))
-	 :nmvi "s-2" (λ! (+workspace/switch-to 1))
-	 :nmvi "s-3" (λ! (+workspace/switch-to 2))
-	 :nmvi "s-4" (λ! (+workspace/switch-to 3))
-	 :nmvi "s-5" (λ! (+workspace/switch-to 4))
-	 :nmvi "s-6" (λ! (+workspace/switch-to 5))
-	 :nmvi "s-7" (λ! (+workspace/switch-to 6))
-	 :nmvi "s-8" (λ! (+workspace/switch-to 7))
-	 :nmvi "s-9" (λ! (+workspace/switch-to 8)))
+   :nmvi "s-t" #'+workspace/new
+   :nmvi "s-1" (λ! (+workspace/switch-to 0))
+   :nmvi "s-2" (λ! (+workspace/switch-to 1))
+   :nmvi "s-3" (λ! (+workspace/switch-to 2))
+   :nmvi "s-4" (λ! (+workspace/switch-to 3))
+   :nmvi "s-5" (λ! (+workspace/switch-to 4))
+   :nmvi "s-6" (λ! (+workspace/switch-to 5))
+   :nmvi "s-7" (λ! (+workspace/switch-to 6))
+   :nmvi "s-8" (λ! (+workspace/switch-to 7))
+   :nmvi "s-9" (λ! (+workspace/switch-to 8)))
 
  :leader
  ;; toggle theme
  (:prefix "t"
-	 :desc "toggle-theme" :n "t" #'+jdkschang/toggle-theme)
+   :desc "toggle-theme" :n "t" #'+jdkschang/toggle-theme)
  ;; ranger
  (:prefix "a"
-	 :desc "Ranger" :n "r" #'ranger
-	 :desc "Deer" :n "d" #'deer)
+   :desc "Ranger" :n "r" #'ranger
+   :desc "Deer" :n "d" #'deer)
  ;; origami folds
  (:prefix "z"
-																				; :map origami-mode-map
-	 :desc "origami-recursively-toggle-node" :nv "z" #'origami-recursively-toggle-node
-	 :desc "origami-recursively-toggle-node" :i "C-z" #'origami-recursively-toggle-node
-	 :desc "origami-toggle-node" :nv "z" #'origami-toggle-node
-	 :desc "origami-toggle-node" :i "C-Z" #'origami-toggle-node
-	 :desc "origami-show-only-node" :nv "s-z" #'origami-show-only-node)
+   ;; :map origami-mode-map
+   :desc "origami-recursively-toggle-node" :nv "z" #'origami-recursively-toggle-node
+   :desc "origami-recursively-toggle-node" :i "C-z" #'origami-recursively-toggle-node
+   :desc "origami-toggle-node" :nv "z" #'origami-toggle-node
+   :desc "origami-toggle-node" :i "C-Z" #'origami-toggle-node
+   :desc "origami-show-only-node" :nv "s-z" #'origami-show-only-node)
  (:prefix "n"
-	 :desc "Browse project notes" :n "p" #'+jdkschang/find-notes-for-project)
+   :desc "Browse project notes" :n "p" #'+jdkschang/find-notes-for-project)
  (:prefix "o"
-	 :desc "eshell in popup" :n "e" #'+eshell/open-popup
-	 :desc "eshell" :n "E" #'+eshell/open))
+   :desc "eshell in popup" :n "e" #'+eshell/open-popup
+   :desc "eshell" :n "E" #'+eshell/open))
